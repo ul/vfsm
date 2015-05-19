a1518f908fa3cd7d28f6f213d044ad4fc1399949
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-05-19 08:31:43 +0300

    up deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure             "1.7.0-beta2" :scope "provided"]
+  :dependencies [[org.clojure/clojure             "1.7.0-beta3" :scope "provided"]
                  [com.github.kyleburton/clj-xpath "1.4.4"]
                  [camel-snake-kebab               "0.3.1"]
                  [instaparse                      "1.4.0"]
-                 [adzerk/boot-cljs                "0.0-2814-4"  :scope "test"]
+                 [adzerk/boot-cljs                "0.0-3269-0"  :scope "test"]
                  [adzerk/bootlaces                "0.1.11"      :scope "test"]])
